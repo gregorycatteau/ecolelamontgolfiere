f134e08ff621587df532b32961e880c4659e9605
striker <striker7333@hotmail.com>
2023-05-14 17:16:02 +0200

    correction chemin acces sendEmail

--- netlify/edge-functions/function_email_reforwarding/function_email_reforwarding.js
+++ netlify/edge-functions/function_email_reforwarding/function_email_reforwarding.js
@@ -1,4 +1,4 @@
-import sendEmail from '@/utils/sendEmail'
+import sendEmail from './utils/sendEmail'
 
 exports.handler = async (event, context) => {
   const { data: contactData } = JSON.parse(event.body);
