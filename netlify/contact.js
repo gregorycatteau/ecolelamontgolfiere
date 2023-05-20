4c2ee58a53b56a459f28826b0b761c1b2e6bc71e
striker <striker7333@hotmail.com>
2023-05-20 22:26:19 +0200

    change-contactjs_to_snippet_one

--- netlify/contact.js
+++ netlify/contact.js
@@ -1,49 +1,14 @@
-import fetch from 'node-fetch';
+import { sendEmail } from "@netlify/emails";
 
-export default async function (event) {
-  if (event.body === null) {
-    return {
-      statusCode: 400,
-      body: JSON.stringify('Payload required'),
-    };
-  }
-
-  const { nom, prénom, email, message } = JSON.parse(event.body);
-
-  const emailOptions = {
-    from: 'association@ecolelamontgolfiere.fr',
-    to: 'association@ecolelamontgolfiere.fr',
-    subject: 'Nouvelle demande de contact',
-    template: 'emails/contact/index.html', 
-    parameters: {
-      nom: nom,
-      prénom: prénom,
-      email: email,
-      message: message,
-    },
-  };
-
-  try {
-    const response = await fetch(`${process.env.URL}/.netlify/functions/emails/contact`, {
-      headers: {
-        'netlify-emails-secret': process.env.NETLIFY_EMAILS_SECRET,
-      },
-      method: 'POST',
-      body: JSON.stringify(emailOptions),
-    });
-
-    if (response.ok) {
-      return {
-        statusCode: 200,
-        body: JSON.stringify('E-mail envoyé avec succès'),
-      };
-    } else {
-      throw new Error("Une erreur s'est produite lors de l'envoi de l'e-mail");
-    }
-  } catch (error) {
-    return {
-      statusCode: 500,
-      body: error.message,
-    };
-  }
-}
+await sendEmail({
+  from: "",
+  to: "",
+  subject: "",
+  template: "contact",
+  parameters: {
+    nom: "", 
+    prénom: "", 
+    email: "", 
+    message: ""
+  },
+});
