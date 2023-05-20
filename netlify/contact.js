4abc3fbe7616fb0eec0fdfef2a99deacbbdd7bcc
striker <striker7333@hotmail.com>
2023-05-20 23:43:37 +0200

    contactjschanged

--- netlify/contact.js
+++ netlify/contact.js
@@ -1,20 +1,20 @@
-await fetch(
-  `${process.env.URL}/.netlify/functions/emails/contact`,
-  {
-    headers: {
-      "netlify-emails-secret": process.env.NETLIFY_EMAILS_SECRET,
-    },
-    method: "POST",
-    body: JSON.stringify({
-      from: "",
-      to: "",
-      subject: "",
-      parameters: {
-        nom: "", 
-        prénom: "", 
-        email: "", 
-        message: ""
-      },
-    }),
+import { sendEmail } from "@netlify/emails";
+exports.handler = async (event, context) => {
+
+await sendEmail({
+  from: "association@ecolelamontgolfiere.fr",
+  to: "association@ecolelamontgolfiere.fr",
+  subject: "Nouvelle demande de contact",
+  template: "contact",
+  parameters: {
+    nom: "", 
+    prénom: "", 
+    email: "", 
+    message: ""
+  },
+});
+  return {
+    statusCode: 200,
+    body: "Email envoyé",
   }
-);
+}
